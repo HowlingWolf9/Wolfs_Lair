a0d3e51f3c3c46e5163a31979ec0378b2cd2bb14
Athul M V <athul121404@gmail.com>
2024-05-02 19:59:53 +0530

    Update Q2-Linear_Search.c

--- Lab_Cycle_3/Q2-Linear_Search.c
+++ Lab_Cycle_3/Q2-Linear_Search.c
@@ -23,18 +23,19 @@ void main()
 		printf("The element does not exist.");
 }
 /* Algorithm
-Step1: Initialise pos=0
-Step2: Take n as input
-Step3: Repeat steps 4 to 5, FOR i=0 when i<n
-Step4: 	Take arr[i] as input
-Step5: 	Increment i by 1
-Step6: Take elm as input 
-Step7: Initialise i=0 and repeat steps 8 to 11, FOR i<n
-Step8: 	IF (elm==arr[i]), then
-Step9: 		Display position
-Step10:		Set pos=i+1
-Step11:	ENDIF
-Step12: IF (pos==0), then
-Step13:	Display "The element does not exist"
-Step14: STOP
+Step1: START
+Step2: Initialise pos=0
+Step3: Take n as input
+Step4: Repeat steps 4 to 5, FOR i=0 when i<n
+Step5: 	Take arr[i] as input
+Step6: 	Increment i by 1
+Step7: Take elm as input 
+Step8: Initialise i=0 and repeat steps 8 to 11, FOR i<n
+Step9: 	IF (elm==arr[i]), then
+Step10: 		Display position
+Step11:		Set pos=i+1
+Step12:	ENDIF
+Step13: IF (pos==0), then
+Step14:	Display "The element does not exist"
+Step15: STOP
 */
---
+++ Lab_Cycle_3/Q4-Palindrome_or_Not.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void main()
+{
+	char string[20],rev[20];
+	int cmp=0,len=0;
+	printf("Enter the string:");
+	scanf("%s",&string);
+	while (string[len] != '\0')
+			len++;
+	for (int i = len; i > 0; i--)
+		rev[len-i]=string[i-1];
+	for (int i = 0; i < len; ++i)
+		if (rev[i] != string[i])
+		{
+			cmp=1;
+			printf("NOT Palindrome");
+			break;
+		}
+	if (cmp==0)
+		printf("Is Pallindrome");
+}
+/*Algorithm
+Step 1: START
+Step 2: Initialise cmp=0, len=0
+Step 3: Take string as input
+Step 4: Repeat step 5, WHILE (string[len] != ‘\0’)
+Step 5: 	Increment len by 1
+Step 6: Initialise i=len and repeat steps 7 to 8, FOR i>0
+Step 7: 	Set rev[len-i] = string[i-1]
+Step 8: 	Decrement i by 1
+Step 9: Initialise i=0 and repeat steps 7 to 8, FOR i<len
+Step 10: 	IF (rev[i] != string[i])
+Step 11: 		Set cmp=1
+Step 12: 		Display “NOT Palindrome”
+Step 13: 		BREAK the loop and goto step 15
+Step 14:	ENDIF
+Step 15: IF (cmp==0)
+Step 16: 	Display “Is Palindrome”
+Step 17: ENDIF
+Step 18: STOP
+*/
