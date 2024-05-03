3b3bd1e3499ea3b8b8453d95dfbab14ac6542d89
Athul M V <athul121404@gmail.com>
2024-05-03 21:59:27 +0530

    Update Q3-Bubble_Sort.c

--- Lab_Cycle_3/Q3-Bubble_Sort.c
+++ Lab_Cycle_3/Q3-Bubble_Sort.c
@@ -26,11 +26,11 @@ void main()
 /*Algorithm
 Step 1: START
 Step 2: Take n as input
-Step 3: Initialise i=0 and repeat steps 4 to 5, FOR i<n
+Step 3: Initialize i=0 and repeat steps 4 to 5, FOR i<n
 Step 4:	Take arr[i] as input
 Step 5:	Increment i by 1
-Step 6: Initialise i=n-1 and repeat steps 7 to 14, FOR i>0
-Step 7:	Initialise j=0 and repeat steps 8 to 13, FOR j<i
+Step 6: Initialize i=n-1 and repeat steps 7 to 14, FOR i>0
+Step 7:	Initialize j=0 and repeat steps 8 to 13, FOR j<i
 Step 8:		IF (arr[j]>arr[j+1]), then
 Step 9:			Set temp=arr[j]
 Step 10:			Set arr[j]=arr[j+1]
@@ -39,7 +39,7 @@ Step 12:		ENDIF
 Step 13:		Increment j by 1
 Step 14:	Decrement i by 1
 Step 15: Display "Sorted Array:"
-Step 16: Initialise i=0 and repeat steps 17 to 18, FOR i<n
+Step 16: Initialize i=0 and repeat steps 17 to 18, FOR i<n
 Step 17:	Display arr[i]
 Step 18:	Increment i by 1
 Step 19: STOP
