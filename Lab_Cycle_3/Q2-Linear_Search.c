140720d4a979ecc8c9ad68ef14acb7de06e9b69f
Athul M V <athul121404@gmail.com>
2024-05-03 21:58:42 +0530

    Update Q2-Linear_Search.c

--- Lab_Cycle_3/Q2-Linear_Search.c
+++ Lab_Cycle_3/Q2-Linear_Search.c
@@ -24,13 +24,13 @@ void main()
 }
 /* Algorithm
 Step 1: START
-Step 2: Initialise pos=0
+Step 2: Initialize pos=0
 Step 3: Take n as input
 Step 4: Repeat steps 4 to 5, FOR i=0 when i<n
 Step 5: 	Take arr[i] as input
 Step 6: 	Increment i by 1
 Step 7: Take elm as input 
-Step 8: Initialise i=0 and repeat steps 8 to 11, FOR i<n
+Step 8: Initialize i=0 and repeat steps 8 to 11, FOR i<n
 Step 9: 	IF (elm==arr[i]), then
 Step 10: 		Display position
 Step 11:		Set pos=i+1
