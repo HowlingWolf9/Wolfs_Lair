1218583ea0b4584b4212281dec79928df4a937ef
Athul M V <athul121404@gmail.com>
2024-05-03 21:56:48 +0530

    Update Q1-Array_Sum_and_Average.c

--- Lab_Cycle_3/Q1-Array_Sum_and_Average.c
+++ Lab_Cycle_3/Q1-Array_Sum_and_Average.c
@@ -18,12 +18,12 @@ void main()
 }
 /*Algorithm
 Step 1: START
-Step 2: Initialise sum=0
+Step 2: Initialize sum=0
 Step 3: Take n as input
-Step 4: Initialise i=0 and repeat steps 4 to 5, FOR i<n
+Step 4: Initialize i=0 and repeat steps 4 to 5, FOR i<n
 Step 5: 	Take arr[i] as input
 Step 6: 	Increment i by 1
-Step 7: Initialise i=0 repeat steps 7 to 9, FOR i<n
+Step 7: Initialize i=0 repeat steps 7 to 9, FOR i<n
 Step 8: 	Increment sum by arr[i]
 Step 9: 	Increment i by 1
 Step 10: Display sum and (sum/n)
