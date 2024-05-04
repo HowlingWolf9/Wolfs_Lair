77bbddc240f9845b352fce124c8d4144d621078d
Athul M V <athul121404@gmail.com>
2024-05-04 22:32:20 +0530

    Update Q3-Bubble_Sort.c

--- Lab_Cycle_3/Q3-Bubble_Sort.c
+++ Lab_Cycle_3/Q3-Bubble_Sort.c
@@ -24,15 +24,15 @@ void main()
 		printf("%d ",arr[i]);
 }
 /*Algorithm
-Step 1: START
-Step 2: Take n as input
-Step 3: Initialize i=0 and repeat steps 4 to 5, FOR i<n
-Step 4:	Take arr[i] as input
-Step 5:	Increment i by 1
-Step 6: Initialize i=n-1 and repeat steps 7 to 14, FOR i>0
-Step 7:	Initialize j=0 and repeat steps 8 to 13, FOR j<i
-Step 8:		IF (arr[j]>arr[j+1]), then
-Step 9:			Set temp=arr[j]
+Step 01: START
+Step 02: Take n as input
+Step 03: Initialize i=0 and repeat steps 4 to 5, FOR i<n
+Step 04:	Take arr[i] as input
+Step 05:	Increment i by 1
+Step 06: Initialize i=n-1 and repeat steps 7 to 14, FOR i>0
+Step 07:	Initialize j=0 and repeat steps 8 to 13, FOR j<i
+Step 08:		IF (arr[j]>arr[j+1]), then
+Step 09:			Set temp=arr[j]
 Step 10:			Set arr[j]=arr[j+1]
 Step 11:			Set arr[j+1]=temp
 Step 12:		ENDIF
