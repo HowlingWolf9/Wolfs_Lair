50149ab96342922339cf9c5c289fcce9131cda35
Athul M V <athul121404@gmail.com>
2024-05-05 11:26:04 +0530

    Update Q8-Data_of_N_Employees.c

--- Lab_Cycle_3/Q8-Data_of_N_Employees.c
+++ Lab_Cycle_3/Q8-Data_of_N_Employees.c
@@ -16,7 +16,7 @@ void main()
 	{
 		printf("\nEmployee No.%d\n---------------\n",i+1);
 		printf("Enter Name :");
-		scanf("%s",&e[i].name);
+		scanf("%s",e[i].name);
 		printf("Enter Id :");
 		scanf("%d",&e[i].id);
 		printf("Enter Salary :");
@@ -28,3 +28,16 @@ void main()
 		printf("%d\t%s\t%d\t%d\n",i+1,e[i].name,e[i].id,e[i].salary);
 	}
 }
+/*Algorithm
+Step 01: START
+Step 02: Declare a structure named employee with id, salary and name as variables
+Step 03: Take n as input
+Step 04: Declare variables e[n] of type struct employee 
+Step 05: Initialize i=0 and repeat steps 6 to 7, FOR i<n
+Step 06: 	Take e[i].name, e[i].id and e[i].salary as input
+Step 07: 	Increment i by 1
+Step 08: Initialize i=0 and repeat steps 6 to 7, FOR i<n
+Step 09: 	Display e[i].name, e[i].id and e[i].salary
+Step 10:	Increment i by 1
+Step 11: STOP
+*/
