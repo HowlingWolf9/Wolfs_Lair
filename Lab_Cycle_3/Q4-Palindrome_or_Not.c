715f4d56c9d734e8e025eb24198f5dde7f6086de
Athul M V <athul121404@gmail.com>
2024-05-06 21:24:34 +0530

    Update Q4-Palindrome_or_Not.c

--- Lab_Cycle_3/Q4-Palindrome_or_Not.c
+++ Lab_Cycle_3/Q4-Palindrome_or_Not.c
@@ -1,3 +1,30 @@
+/*
+Experiment: 24
+Date: 01/05/2024
+
+AIM: Write a C program to read a string (word), store it in an array and check whether it is a palindrome word or not.
+
+ALGORITHM:
+Step 01: START
+Step 02: Initialize cmp=0, len=0
+Step 03: Take string as input
+Step 04: Repeat step 5, WHILE (string[len] != ‘\0’)
+Step 05: 	Increment len by 1
+Step 06: Initialize i=len and repeat steps 7 to 8, FOR i>0
+Step 07: 	Set rev[len-i] = string[i-1]
+Step 08: 	Decrement i by 1
+Step 09: Initialize i=0 and repeat steps 10 to 14, FOR i<len
+Step 10: 	IF (rev[i] != string[i]), then
+Step 11: 		Set cmp=1
+Step 12: 		Display “NOT Palindrome”
+Step 13: 		BREAK the loop and go to step 15
+Step 14:	ENDIF
+Step 15: IF (cmp==0)
+Step 16: 	Display “Is Palindrome”
+Step 17: ENDIF
+Step 18: STOP
+*/
+//CODE:
 #include <stdio.h>
 
 void main()
@@ -20,23 +47,3 @@ void main()
 	if (cmp==0)
 		printf("Is Palindrome");
 }
-/*Algorithm
-Step 01: START
-Step 02: Initialize cmp=0, len=0
-Step 03: Take string as input
-Step 04: Repeat step 5, WHILE (string[len] != ‘\0’)
-Step 05: 	Increment len by 1
-Step 06: Initialize i=len and repeat steps 7 to 8, FOR i>0
-Step 07: 	Set rev[len-i] = string[i-1]
-Step 08: 	Decrement i by 1
-Step 09: Initialize i=0 and repeat steps 10 to 14, FOR i<len
-Step 10: 	IF (rev[i] != string[i]), then
-Step 11: 		Set cmp=1
-Step 12: 		Display “NOT Palindrome”
-Step 13: 		BREAK the loop and go to step 15
-Step 14:	ENDIF
-Step 15: IF (cmp==0)
-Step 16: 	Display “Is Palindrome”
-Step 17: ENDIF
-Step 18: STOP
-*/
