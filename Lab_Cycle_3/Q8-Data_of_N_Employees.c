cc05ff30aec7e36a55ddb6f5c85e7bfc1908bae9
Athul M V <athul121404@gmail.com>
2024-05-06 21:04:59 +0530

    Update Q8-Data_of_N_Employees.c

--- Lab_Cycle_3/Q8-Data_of_N_Employees.c
+++ Lab_Cycle_3/Q8-Data_of_N_Employees.c
@@ -1,3 +1,22 @@
+/*
+Experiment: 28
+
+AIM: Write a C program, and using structure, read and print data of n employees (Name, Employee Id and Salary)
+
+ALGORITHM:
+Step 01: START
+Step 02: Declare a structure named employee with id, salary and name as variables
+Step 03: Take n as input
+Step 04: Declare variables e[n] of type struct employee 
+Step 05: Initialize i=0 and repeat steps 6 to 7, FOR i<n
+Step 06: 	Take e[i].name, e[i].id and e[i].salary as input
+Step 07: 	Increment i by 1
+Step 08: Initialize i=0 and repeat steps 6 to 7, FOR i<n
+Step 09: 	Display e[i].name, e[i].id and e[i].salary
+Step 10:	Increment i by 1
+Step 11: STOP
+*/
+//CODE:
 #include <stdio.h>
 
 struct employee
@@ -28,16 +47,3 @@ void main()
 		printf("%d\t%s\t%d\t%d\n",i+1,e[i].name,e[i].id,e[i].salary);
 	}
 }
-/*Algorithm
-Step 01: START
-Step 02: Declare a structure named employee with id, salary and name as variables
-Step 03: Take n as input
-Step 04: Declare variables e[n] of type struct employee 
-Step 05: Initialize i=0 and repeat steps 6 to 7, FOR i<n
-Step 06: 	Take e[i].name, e[i].id and e[i].salary as input
-Step 07: 	Increment i by 1
-Step 08: Initialize i=0 and repeat steps 6 to 7, FOR i<n
-Step 09: 	Display e[i].name, e[i].id and e[i].salary
-Step 10:	Increment i by 1
-Step 11: STOP
-*/
