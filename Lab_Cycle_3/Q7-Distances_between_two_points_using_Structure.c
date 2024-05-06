3843e3be03ed15e65ff5cbbfd08940bd2bb8ff8e
Athul M V <athul121404@gmail.com>
2024-05-06 21:44:30 +0530

    Update Q7-Distances_between_two_points_using_Structure.c

--- Lab_Cycle_3/Q7-Distances_between_two_points_using_Structure.c
+++ Lab_Cycle_3/Q7-Distances_between_two_points_using_Structure.c
@@ -1,3 +1,20 @@
+/*
+Experiment: 27
+Date:
+
+AIM: Write a C program to read two input each representing the distances between two points in the Euclidean space, store these in structure variables and add the two distance values.
+
+ALGORITHM:
+Step 01: START
+Step 02: Declare a structure named coords with x and y as variables
+Step 03: Declare variables c1 and c2 of type struct coords
+Step 04: Take c1.x and c1.y input 
+Step 05: Take c2.x and c2.y input
+Step 06: Set dist=√(((c2.x-c1.x)^2)+((c2.y-c1.y)^2))
+Step 07: Display dist
+Step 08: STOP
+*/
+//CODE:
 #include <stdio.h>
 #include <math.h>
 
@@ -16,13 +33,3 @@ void main()
 	float dist=(sqrt(pow((c2.x-c1.x),2)+pow((c2.y-c1.y),2)));
 	printf("The distance between the points = %.3f",dist);
 }
-/*Algorithm
-Step 01: START
-Step 02: Declare a structure named coords with x and y as variables
-Step 03: Declare variables c1 and c2 of type struct coords
-Step 04: Take c1.x and c1.y input 
-Step 05: Take c2.x and c2.y input
-Step 06: Set dist=√(((c2.x-c1.x)^2)+((c2.y-c1.y)^2))
-Step 07: Display dist
-Step 08: STOP
-*/
