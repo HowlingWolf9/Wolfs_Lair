80b4f7056e7c606cdd93c1f9083cd7a13e13fb26
Athul M V <athul121404@gmail.com>
2024-05-06 21:44:22 +0530

    Update Q5-Concatination_of_Strings.c

--- Lab_Cycle_3/Q5-Concatination_of_Strings.c
+++ Lab_Cycle_3/Q5-Concatination_of_Strings.c
@@ -1,3 +1,28 @@
+/*
+Experiment: 25
+Date:
+
+AIM: Write a C program to read two strings (each one ending with a $ symbol), store them in arrays and concatenate them without using library functions.
+
+ALGORITHM:
+Step 01: START
+Step 02: Initialize len1=0, len2=0
+Step 03: Take str1 and str2 as input
+Step 04: Repeat step 5, WHILE (str1[len1] != ‘$’)
+Step 05: 	Increment len1 by 1
+Step 06: Repeat step 7, WHILE (str2[len2] != ‘$’)
+Step 07: 	Increment len2 by 1
+Step 08: Set clen=len1+len2
+Step 09: Initialize i=0 and repeat steps 10 to 11, FOR i<len1
+Step 10: 	Set combined[i]=str1[i]
+Step 11: 	Increment i by 1
+Step 12: Initialize n=0 and repeat steps 10 to 11, FOR i<clen
+Step 13: 	Set combined[i]=str2[n]
+Step 14: 	Increment i and n by 1
+Step 15: Display combined
+Step 16: STOP
+*/
+//CODE:
 #include <stdio.h>
 
 void main()
@@ -19,21 +44,3 @@ void main()
 		combined[i]=str2[n];
 	printf("%s",combined);
 }
-/*Algorithm
-Step 01: START
-Step 02: Initialize len1=0, len2=0
-Step 03: Take str1 and str2 as input
-Step 04: Repeat step 5, WHILE (str1[len1] != ‘$’)
-Step 05: 	Increment len1 by 1
-Step 06: Repeat step 7, WHILE (str2[len2] != ‘$’)
-Step 07: 	Increment len2 by 1
-Step 08: Set clen=len1+len2
-Step 09: Initialize i=0 and repeat steps 10 to 11, FOR i<len1
-Step 10: 	Set combined[i]=str1[i]
-Step 11: 	Increment i by 1
-Step 12: Initialize n=0 and repeat steps 10 to 11, FOR i<clen
-Step 13: 	Set combined[i]=str2[n]
-Step 14:	Increment i and n by 1
-Step 15: Display combined
-Step 16: STOP
-*/
