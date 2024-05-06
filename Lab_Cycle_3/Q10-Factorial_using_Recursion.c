cf9ed46a310901baf5201ef3740eb8da5e78370e
Athul M V <athul121404@gmail.com>
2024-05-06 23:24:20 +0530

    Update Q10-Factorial_using_Recursion.c

--- Lab_Cycle_3/Q10-Factorial_using_Recursion.c
+++ Lab_Cycle_3/Q10-Factorial_using_Recursion.c
@@ -1,3 +1,23 @@
+/*
+Experiment: 30
+Date: 
+
+AIM: Write a C program to find the factorial of a given Natural Number n using recursive function
+ALGORITHM:
+Step 01: START
+Step 02: Declare a function fact with return type int and argument (int num)
+Step 03: Define fact
+Step 04: 	IF (num<=0), then
+Step 05: 		RETURN 0
+Step 06: 	ELSE IF (num==1), then
+Step 07: 		RETURN 1
+Step 08: 	ENDIF
+Step 09: 	RETURN num*fact(num-1)
+Step 10: Take num as input
+Step 11: Display fact(num)
+Step 12: STOP
+*/
+//CODE:
 #include <stdio.h>
 
 int fact(int num)
@@ -7,7 +27,6 @@ int fact(int num)
 	else if (num==1)
 		return 1;
 	return num*fact(num-1);
-	
 }
 
 void main()
