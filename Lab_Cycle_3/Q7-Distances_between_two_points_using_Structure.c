4550e13cd06fec7c5b1cb5e4b155b18e684c7db7
Athul M V <athul121404@gmail.com>
2024-05-09 02:26:42 +0530

    Update Q7-Distances_between_two_points_using_Structure.c

--- Lab_Cycle_3/Q7-Distances_between_two_points_using_Structure.c
+++ Lab_Cycle_3/Q7-Distances_between_two_points_using_Structure.c
@@ -19,17 +19,23 @@ Step 08: STOP
 #include <math.h>
 
 struct coords
-	{
-		int x,y;
-	};
+{
+	int x,y;
+};
 		
 void main()
 {
 	struct coords c1,c2;
-	printf("Enter the Co-Ordinates of 1st (x,y) point :");
+	printf("Enter the Co-Ordinates of 1st  point (x,y): ");
 	scanf("%d%d",&c1.x,&c1.y);
-	printf("Enter the Co-Ordinates of 2nd (x,y) point :");
+	printf("Enter the Co-Ordinates of 2nd  point (x,y): ");
 	scanf("%d%d",&c2.x,&c2.y);
 	float dist=(sqrt(pow((c2.x-c1.x),2)+pow((c2.y-c1.y),2)));
 	printf("The distance between the points = %.3f",dist);
 }
+/*
+OUTPUT:
+Enter the Co-Ordinates of 1st  point (x,y): 4 5
+Enter the Co-Ordinates of 2nd  point (x,y): 5 4
+The distance between the points = 1.414
+*/
