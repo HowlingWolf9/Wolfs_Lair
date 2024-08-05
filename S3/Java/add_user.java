b3b178ccec58d22bcc0e193417595431c284d9a1
Athul M V <athul121404@gmail.com>
2024-08-05 20:40:06 +0530

    Create add_user.java

---
+++ S3/Java/add_user.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.*;
+class add_user
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the 1st Number:");
+		int a=scan.nextInt();
+		System.out.println("Enter the 2nd Number:");
+		int b=scan.nextInt();
+		int c=a+b;
+		System.out.println("c="+c);
+	}
+}
