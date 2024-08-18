9e1f4082218b74b06e55ea2be381a590d3eb3eb2
Athul M V <athul121404@gmail.com>
2024-08-19 00:29:03 +0530

    Create Sum_of_Diagonals.java

---
+++ S3/Java/Sum_of_Diagonals.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Sum_of_Diagonals
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter the size of the Matrix:");
+    int dim= scan.nextInt();
+    int mat[][]=new int[dim][dim];
+    System.out.println("Enter the Elements of Matrix 1:");
+    for ( int i=0 ; i<dim ; i++ )
+      for ( int j=0 ; j<dim ; j++ )
+        mat[i][j]=scan.nextInt();
+    int sum=0;
+    for ( int i=0 ; i<dim ; i++ )
+        sum+=mat[i][i];
+    System.out.println("Sum="+sum);
+    }
+}
