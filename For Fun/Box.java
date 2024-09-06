3a55916a00d0a94bd076bb2367bb42b756635801
Athul M V <athul121404@gmail.com>
2024-09-06 18:53:47 +0530

    Update Box.java

--- For Fun/Box.java
+++ For Fun/Box.java
@@ -3,14 +3,16 @@ class Box
     int length;
     int width;
     int height;
-    void Box(int length, int width, int height)
+    Box(int length, int width, int height)
     {
         this.length = length;
         this.width = width;
         this.height = height;
     }
-
-    public static void main()
+}
+class BoxDemo
+{
+    public static void main(String args[])
     {
         Box myBox = new Box(10,15,20);
         System.out.println("Length:"+myBox.length);
