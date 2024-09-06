b8c77b0b4a0e566c73307ccc952dee680f712843
Athul M V <athul121404@gmail.com>
2024-09-06 22:28:42 +0530

    Create Q6.c

---
+++ NPTEL/Problem Solving Through Programming in C/Week_7/Q6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char p[] = "assignment";
+    char t;
+    int i,j;
+    for (i = 0, j = strlen(p); i<j; i++)
+    {
+        t = p[i];
+        p[i] = p[j - i];
+        p[j - i] = t;
+    }
+    printf("%s", p);
+    return 0;
+}
