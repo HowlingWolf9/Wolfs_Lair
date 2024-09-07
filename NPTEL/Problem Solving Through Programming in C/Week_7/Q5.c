05e882e1af6146fd9918ff36ed7a648c6ec10e89
Athul M V <athul121404@gmail.com>
2024-09-07 19:43:26 +0530

    Update Q5.c

--- NPTEL/Problem Solving Through Programming in C/Week_7/Q5.c
+++ NPTEL/Problem Solving Through Programming in C/Week_7/Q5.c
@@ -6,3 +6,11 @@ int main()
     printf("%d\n", ary[1][0]);
     return 0;
 }
+/*
+OUTPUT:
+:5:9: error: expected expression before ']' token
+    5 |     ary[][] = {{1,2,3},{4,5,6}};
+      |         ^
+
+Build finished with error(s).
+*/
