4993f1ebd0cac062a68719d3ff742f69367a0a3d
Athul M V <athul121404@gmail.com>
2024-09-22 19:56:00 +0530

    Create P1.c

---
+++ NPTEL/Problem Solving Through Programming in C/Week_10/P1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ int main()
+{
+    int a[3][3], i, j;
+    long determinant;
+ //  9 elements of matrix is taken as input from test data
+    for(i = 0 ; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            scanf("%d", &a[i][j]);
+
+/*Use the printf statements as:
+printf("The given matrix is not invertible");
+printf("The given matrix is invertible"); 
+*/
+	determinant = a[0][0]*((a[1][1]*a[2][2])-(a[1][2]*a[2][1]));
+    printf("%ld\n", determinant);
+	determinant -= a[0][1]*((a[1][0]*a[2][2])-(a[2][0]*a[1][2]));
+    printf("%ld\n", determinant);
+	determinant += a[0][2]*((a[1][0]*a[2][1])-(a[2][0]*a[1][1]));
+    printf("%ld\n", determinant);
+	if (determinant == 0)
+      printf("The given matrix is not invertible");
+	else
+      printf("The given matrix is invertible");
+}
