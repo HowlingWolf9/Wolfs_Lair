37e93b0281cca32947bb7550401204445cb0f3e3
Athul M V <athul121404@gmail.com>
2024-10-27 18:26:31 +0530

    Update Q1.c

--- S3/Data_Structure/Cycle_1/Q1.c
+++ S3/Data_Structure/Cycle_1/Q1.c
@@ -1,8 +1,35 @@
+/*
+Experiment: 1
+
+AIM: Write a program to perform Linear search on n numbers. Write Linear Search as a function and call it from the function main() by passing the appropriate parameters. 
+
+ALGORITHM: main()
+Step 01: START
+Step 02: Read the elements into the array, arr[]
+Step 03: Call the linearSearch() function
+Step 04: END
+
+ALGORITHM: linearSearch()
+Step 01: START
+Step 02: Initialize the key variable to 0
+Step 03: Read the element to be searched, elm
+Step 04: Initialize i = 0 and repeat steps 05 to 07 until i < SIZE
+Step 05:     IF arr[i] == elm
+Step 06:         Print the position of the element
+Step 07:         Set key = 1
+Step 08:     ENDIF
+Step 08:     Increment i by 1
+Step 09: IF key == 0
+Step 10:     Print the message "Element does not exist"
+Step 11: ENDIF
+Step 12: STOP
+*/
+//CODE:
 #include <stdio.h>
 #define SIZE 5
 int arr[SIZE];
 
-int linear_search()
+int linearSearch()
 {
     int key=0,elm;
     printf("Enter the element to search for:");
@@ -22,5 +49,12 @@ void main()
     printf("Enter the %d elements in the array:",SIZE);
     for (int i = 0; i < SIZE; i++)
         scanf("%d",&arr[i]);
-    linear_search();
+    linearSearch();
 }
+
+/*
+OUTPUT:
+Enter the 5 elements in the array: 1 2 3 4 5
+Enter the element to search for: 3
+Element is at position 3
+*/
