52b2ca9e38a17a546d2cc5becaee065256079b0d
Athul M V <athul121404@gmail.com>
2024-10-31 21:52:37 +0530

    Update DivisionExample.java

--- S3/Java/Lab Cycle/Q6/DivisionExample.java
+++ S3/Java/Lab Cycle/Q6/DivisionExample.java
@@ -1,3 +1,29 @@
+/*
+Experiment: 6
+
+AIM: Write a Java program that reads an integer input from the user. The program should attempt to divide 100 by this integer and display the result. However, if the user enters a zero, the program should catch the ArithmeticException and display an appropriate error message. Additionally, if the user inputs a non-integer value, catch the InputMismatchException and display an error message.Regardless of whether an exception is thrown or not, the program should always print a message in the finally block that says "Execution completed."
+
+ALGORITHM:
+In try block
+{
+    Read an integer input from the user, userInput
+    Compute result = 100 / userInput
+    Display result
+}
+Catch ArithmeticException
+{
+    Display "Error: Cannot divide by zero."
+}
+Catch InputMismatchException
+{
+    Display "Error: Please enter a valid integer."
+}
+Finally
+{
+    Display "Execution completed."
+}
+*/
+//CODE:
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
