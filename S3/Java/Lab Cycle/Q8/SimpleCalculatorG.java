eda41d4b01809170299859ae1ad28f0e623c53d3
Athul M V <athul121404@gmail.com>
2024-11-02 19:21:56 +0530

    Update SimpleCalculatorG.java

--- S3/Java/Lab Cycle/Q8/SimpleCalculatorG.java
+++ S3/Java/Lab Cycle/Q8/SimpleCalculatorG.java
@@ -1,3 +1,4 @@
+
 /*
 Experiment: 8
 
@@ -29,12 +30,11 @@ class NumberButtonListener implements ActionListener {
         this.c = c;
     }
 
-public void actionPerformed(ActionEvent e)
-{
-JButton button = (JButton) e.getSource();
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
 
-c.calculationField.setText(c.calculationField.getText() + button.getText());
-}
+        c.calculationField.setText(c.calculationField.getText() + button.getText());
+    }
 }
 
 class OperatorButtonListener implements ActionListener {
@@ -69,132 +69,128 @@ class ButtonEquals implements ActionListener {
         this.c = c;
     }
 
-public void actionPerformed(ActionEvent e)
-{
-String input = c.calculationField.getText();
-String[] tokens = input.split(" ");
-if(tokens.length != 3)
-{
-c.calculationField.setText("Error: Invalid input");
-return;
-}
-try
-{
-float operand1 = Integer.parseInt(tokens[0]);
-float operand2 = Integer.parseInt(tokens[2]);
+    public void actionPerformed(ActionEvent e) {
+        String input = c.calculationField.getText();
+        String[] tokens = input.split(" ");
+        if (tokens.length != 3) {
+            c.calculationField.setText("Error: Invalid input");
+            return;
+        }
+        try {
+            float operand1 = Integer.parseInt(tokens[0]);
+            float operand2 = Integer.parseInt(tokens[2]);
 
-String operator = tokens[1];
-float result = 0;
-switch (operator)
-{
-case "+":
-result = operand1 + operand2;
-break;
-case "-":
-result = operand1 - operand2;
-break;
-case "*":
-result = operand1 * operand2;
-break;
-case "/":
-if (operand2 == 0) {
-c.calculationField.setText("Error: Division by zero");
-return;
-}
-result = operand1 / operand2;
-break;
-default:
-c.calculationField.setText("Error: Invalid operator");
-return;
-}// End switch
-c.calculationField.setText(String.valueOf(result));
-} catch (NumberFormatException ex) {
-c.calculationField.setText("Error: Invalid number format");
-}//End try-catch
-}// End actionPerformed()
+            String operator = tokens[1];
+            float result = 0;
+            switch (operator) {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                case "/":
+                    if (operand2 == 0) {
+                        c.calculationField.setText("Error: Division by zero");
+                        return;
+                    }
+                    result = operand1 / operand2;
+                    break;
+                default:
+                    c.calculationField.setText("Error: Invalid operator");
+                    return;
+            }// End switch
+            c.calculationField.setText(String.valueOf(result));
+        } catch (NumberFormatException ex) {
+            c.calculationField.setText("Error: Invalid number format");
+        } // End try-catch
+    }// End actionPerformed()
 }// End class
 
-class CalculatorGUI{
-JTextField calculationField ;
-JFrame frame ;
-JButton button1 ;
-JButton button2 ;
-JButton button3 ;
-JButton button4 ;
-JButton button5 ;
-JButton button6;
-JButton button7 ;
-JButton button8 ;
-JButton button9 ;
-JButton button0 ;
-JButton buttonClear ;
-JButton buttonEquals ;
-JButton buttonAdd ;
-JButton buttonSubtract;
-JButton buttonMultiply ;
-JButton buttonDivide ;
+class CalculatorGUI {
+    JTextField calculationField;
+    JFrame frame;
+    JButton button1;
+    JButton button2;
+    JButton button3;
+    JButton button4;
+    JButton button5;
+    JButton button6;
+    JButton button7;
+    JButton button8;
+    JButton button9;
+    JButton button0;
+    JButton buttonClear;
+    JButton buttonEquals;
+    JButton buttonAdd;
+    JButton buttonSubtract;
+    JButton buttonMultiply;
+    JButton buttonDivide;
 
-CalculatorGUI(){
-frame = new JFrame("Calculator");
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.setSize(225, 200);
-frame.setLayout(new FlowLayout());
-frame.setVisible(true);
-calculationField = new JTextField(20);
-button1 = new JButton("1");
-button2 = new JButton("2");
-button3 = new JButton("3");
-button4 = new JButton("4");
-button5 = new JButton("5");
-button6 = new JButton("6");
-button7 = new JButton("7");
-button8 = new JButton("8");
-button9 = new JButton("9");
-button0 = new JButton("0");
-buttonClear = new JButton("C");
-buttonEquals = new JButton("=");
-buttonAdd = new JButton("+");
-buttonSubtract = new JButton("-");
-buttonMultiply = new JButton("*");
-buttonDivide = new JButton("/");
-frame.add(calculationField);
-frame.add(button1);
-frame.add(button2);
-frame.add(button3);
-frame.add(buttonClear);
-frame.add(button4);
-frame.add(button5);
-frame.add(button6);
-frame.add(buttonAdd);
-frame.add(button7);
-frame.add(button8);
-frame.add(button9);
-frame.add(buttonSubtract);
-frame.add(button0);
-frame.add(buttonEquals);
-frame.add(buttonMultiply);
-frame.add(buttonDivide);
-NumberButtonListener numberButtonListener = new NumberButtonListener(this);
-button1.addActionListener(numberButtonListener);
-button2.addActionListener(numberButtonListener);
-button3.addActionListener(numberButtonListener);
-button4.addActionListener(numberButtonListener);
-button5.addActionListener(numberButtonListener);
-button6.addActionListener(numberButtonListener);
-button7.addActionListener(numberButtonListener);
-button8.addActionListener(numberButtonListener);
-button9.addActionListener(numberButtonListener);
-button0.addActionListener(numberButtonListener);
-OperatorButtonListener operatorButtonListener=new OperatorButtonListener(this);
-buttonAdd.addActionListener(operatorButtonListener);
-buttonSubtract.addActionListener(operatorButtonListener);
-buttonMultiply.addActionListener(operatorButtonListener);
-buttonDivide.addActionListener(operatorButtonListener);
-ButtonClear buttonClearObj=new ButtonClear(this);
-ButtonEquals buttonEqualsObj = new ButtonEquals(this);
-buttonClear.addActionListener(buttonClearObj);
-buttonEquals.addActionListener(buttonEqualsObj);
-}//End of constructor
+    CalculatorGUI() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(225, 200);
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+        calculationField = new JTextField(20);
+        button1 = new JButton("1");
+        button2 = new JButton("2");
+        button3 = new JButton("3");
+        button4 = new JButton("4");
+        button5 = new JButton("5");
+        button6 = new JButton("6");
+        button7 = new JButton("7");
+        button8 = new JButton("8");
+        button9 = new JButton("9");
+        button0 = new JButton("0");
+        buttonClear = new JButton("C");
+        buttonEquals = new JButton("=");
+        buttonAdd = new JButton("+");
+        buttonSubtract = new JButton("-");
+        buttonMultiply = new JButton("*");
+        buttonDivide = new JButton("/");
+        frame.add(calculationField);
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+        frame.add(buttonClear);
+        frame.add(button4);
+        frame.add(button5);
+        frame.add(button6);
+        frame.add(buttonAdd);
+        frame.add(button7);
+        frame.add(button8);
+        frame.add(button9);
+        frame.add(buttonSubtract);
+        frame.add(button0);
+        frame.add(buttonEquals);
+        frame.add(buttonMultiply);
+        frame.add(buttonDivide);
+        NumberButtonListener numberButtonListener = new NumberButtonListener(this);
+        button1.addActionListener(numberButtonListener);
+        button2.addActionListener(numberButtonListener);
+        button3.addActionListener(numberButtonListener);
+        button4.addActionListener(numberButtonListener);
+        button5.addActionListener(numberButtonListener);
+        button6.addActionListener(numberButtonListener);
+        button7.addActionListener(numberButtonListener);
+        button8.addActionListener(numberButtonListener);
+        button9.addActionListener(numberButtonListener);
+        button0.addActionListener(numberButtonListener);
+        OperatorButtonListener operatorButtonListener = new OperatorButtonListener(this);
+        buttonAdd.addActionListener(operatorButtonListener);
+        buttonSubtract.addActionListener(operatorButtonListener);
+        buttonMultiply.addActionListener(operatorButtonListener);
+        buttonDivide.addActionListener(operatorButtonListener);
+        ButtonClear buttonClearObj = new ButtonClear(this);
+        ButtonEquals buttonEqualsObj = new ButtonEquals(this);
+        buttonClear.addActionListener(buttonClearObj);
+        buttonEquals.addActionListener(buttonEqualsObj);
+    }// End of constructor
 }
 
 public class SimpleCalculatorG {
