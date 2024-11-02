a421a03c9cb8771a99d0a36352a0b54013b565cd
Athul M V <athul121404@gmail.com>
2024-11-02 19:21:50 +0530

    Update MainClass.java

--- S3/Java/Lab Cycle/Q3/MainClass.java
+++ S3/Java/Lab Cycle/Q3/MainClass.java
@@ -1,7 +1,7 @@
 /*
 Experiment: 3
 
-AIM: Write a Java program which creates a class named 'Employee' having the following members: Name, Age, Phone number, Address, Salary. It also has a method named 'printSalary( )' which prints the salary of the Employee. Two classes 'Officer' and 'Manager' inherits the 'Employee' class. The 'Officer' and 'Manager' classes have data members 'specialization' and 'department' respectively. Now, assign name, age, phone number, address and salary to an officer and a manager by making an object of both of these classes and print the same. (Exercise to understand inheritance). ** 
+AIM: Write a Java program which creates a class named 'Employee' having the following members: Name, Age, Phone number, Address, Salary. It also has a method named 'printSalary()' which prints the salary of the Employee. Two classes 'Officer' and 'Manager' inherits the 'Employee' class. The 'Officer' and 'Manager' classes have data members 'specialization' and 'department' respectively. Now, assign name, age, phone number, address and salary to an officer and a manager by making an object of both of these classes and print the same. (Exercise to understand inheritance). ** 
 
 ALGORITHM:
 Define class Employee with public attributes Name and Age, private attributes PhoneNumber, Address and protected attribute Salary.
