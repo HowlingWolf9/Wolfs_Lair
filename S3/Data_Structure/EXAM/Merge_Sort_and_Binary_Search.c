0aac73ef02173b175efa565dda1ab83992952d70
Athul M V <athul121404@gmail.com>
2024-11-15 11:48:10 +0530

    Create Merge_Sort_and_Binary_Search.c

---
+++ S3/Data_Structure/EXAM/Merge_Sort_and_Binary_Search.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+void binarySearch(int arr[], int elm, int n)
+{
+    int left = 0, right = n - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == elm)
+        {
+            printf("Element %d is present at index %d\n", elm, mid);
+            break;
+        }
+        else if (arr[mid] < elm)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    if (left > right)
+        printf("Element %d is not present in the array\n", elm);
+}
+void main()
+{
+    int n, elm;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    mergeSort(arr, 0, n - 1);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\nEnter the element to search for: ");
+    scanf("%d", &elm);
+    binarySearch(arr, elm, n);
+}
