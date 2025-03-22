77cb5b023dcc44a286b7f96c3fe9a05162f48f09
HowlingWolf9 <athul121404@gmail.com>
2025-03-22 07:15:24 +0000

    Add C program to demonstrate usage of getpid() system call

---
+++ S4/Operating_System/Exp4-System_Calls/4c-getpid.c
@@ -0,0 +1,20 @@
+/*
+ALGORITHM:
+STEP 01: Include the necessary header files
+STEP 02: Call getpid() to get the process ID
+STEP 03: Print the process ID
+STEP 04: STOP
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void main()
+{
+    pid_t pid = getpid();
+    printf("Process ID : %d\n", pid);
+}
+/*
+OUTPUT:
+Process ID : 5682
+*/
