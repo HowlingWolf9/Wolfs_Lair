50a127847d7b585f2734a00d5e0a3fcc5279bf25
HowlingWolf9 <athul121404@gmail.com>
2025-03-22 08:59:22 +0000

    Add C program to demonstrate usage of exit() system call

---
+++ S4/Operating_System/Exp4-System_Calls/4d-exit.c
@@ -0,0 +1,18 @@
+/*
+ALGORITHM:
+STEP 01: Include the necessary header files
+STEP 02: Use the exit() system call to terminate the program with a status code 0
+STEP 03: STOP
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    printf("Program will terminate with exit status 0\n");
+    exit(0);
+}
+/*
+OUTPUT:
+Program will terminate with exit status 0
+*/
