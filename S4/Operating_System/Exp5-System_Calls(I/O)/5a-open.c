d9e0ee5325a4465d7233107131fe2e2a3f36b7bc
Athul M V <athul121404@gmail.com>
2025-03-24 18:18:32 +0530

    Add C program to demonstrate file opening using open() system call

---
+++ S4/Operating_System/Exp5-System_Calls(I/O)/5a-open.c
@@ -0,0 +1,28 @@
+/*
+ALGORITHM:
+STEP 01: Include the necessary header files
+STEP 02: Use open() to open a file
+         -  If the file does not exist, create it
+STEP 03: Print the file descriptor returned by open()
+STEP 04: STOP
+*/
+//CODE:
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void main()
+{
+    int fd = open("sample.txt", O_RDWR | O_CREAT, 0644);
+    if (fd == -1)
+        printf("Error opening file\n");
+    else
+    {
+        printf("File opened successfully with file descriptor: %d\n", fd);
+        close(fd);
+    }
+}
+/*
+OUTPUT:
+
+*/
