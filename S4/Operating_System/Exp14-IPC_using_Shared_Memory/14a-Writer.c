beee34ae0b9d1071ce1f14075784cc6820acc129
HowlingWolf9 <athul121404@gmail.com>
2025-04-08 09:09:52 +0000

    Fix shmat argument to use NULL instead of (void *)0 in 14a-Writer.c

--- S4/Operating_System/Exp14-IPC_using_Shared_Memory/14a-Writer.c
+++ S4/Operating_System/Exp14-IPC_using_Shared_Memory/14a-Writer.c
@@ -17,7 +17,7 @@ void main()
 {
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
-    char *str = (char *)shmat(shmid, (void *)0, 0);
+    char *str = (char *)shmat(shmid, NULL, 0);
     printf("Write Data: ");
     gets(str);
     printf("Data written in memory: %s\n", str);
