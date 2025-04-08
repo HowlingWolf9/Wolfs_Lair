1d38477960cb8ce566ac5c1c7009bd8616cd8236
HowlingWolf9 <athul121404@gmail.com>
2025-04-08 09:10:20 +0000

    Fix shmat argument to use NULL instead of (void *)0 in 14b-Reader.c

--- S4/Operating_System/Exp14-IPC_using_Shared_Memory/14b-Reader.c
+++ S4/Operating_System/Exp14-IPC_using_Shared_Memory/14b-Reader.c
@@ -17,7 +17,7 @@ void main()
 {
     key_t key = ftok("shmfile", 65);
     int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
-    char *str = (char *)shmat(shmid, (void *)0, 0);
+    char* str = (char*) shmat(shmid, NULL, 0);
     printf("Data read from memory: %s\n", str);
     shmdt(str);
     shmctl(shmid, IPC_RMID, NULL);
