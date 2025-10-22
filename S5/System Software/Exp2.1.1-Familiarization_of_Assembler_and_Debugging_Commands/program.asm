c6afd820492c2f6ef41c3d4889506e6898812861
Athul M V <athul121404@gmail.com>
2025-10-22 18:46:23 +0530

    feat: add assembly program and DOSBox configuration for "Hello, World!" demonstration

--- S5/System Software/Exp2.1.1-Familiarization_of_Assembler_and_Debugging_Commands/program.asm
+++ S5/System Software/Exp2.1.1-Familiarization_of_Assembler_and_Debugging_Commands/program.asm
@@ -1,16 +1,18 @@
 assume cs:code, ds:data
-data segment
-    mesg db "Hello, World!$"
+data    segment
+        mesg db "Hello, World! $"
 data ends
 
 code segment
-start:
-    mov ax, data
-    mov ds, ax
-    mov dx, offset mesg
-    mov ah, 09h
-    int 21h
-    mov ax, 4ch
-    int 21h
-code ends
-end start
+start:  mov ax, data
+        mov ds, ax
+
+        mov dx, offset mesg
+        mov ah, 09h
+        int 21h
+
+        mov ah, 4ch
+        int 21h
+        
+code    ends
+        end start
