378387df0f4a0a2b104dbe97b5e07b656ed69368
Athul M V <athul121404@gmail.com>
2026-03-14 19:57:53 +0530

    Add HEHE asm/executables; tweak Addition operand
    Add new exam assembly (S5/System Software/Exam/hehe.asm) and its built outputs (HEHE.EXE, HEHE.OBJ). Modify Addition.asm to change opr2 from 0002h to 0FFFFh and include updated ADDITION.EXE/ADDITION.OBJ binaries to reflect the rebuilt changes.

--- S5/System Software/Exam/hehe.asm
+++ S5/System Software/Exam/hehe.asm
@@ -1,24 +1,27 @@
 assume cs:code, ds:data
 data segment
     opr1   dw 1234h
-    opr2   dw 0ffffh
+    opr2   dw 0002h
+    opr3   dw 0003h
     result dw 01h dup(?)
 data ends
-
 code segment
-    start:mov ax, data
+    start:
+          mov ax, data
           mov ds, ax
 
           mov ax, opr1
           mov bx, opr2
-        
+          mov cx, opr3
+
           clc
           add ax, bx
+          add ax, cx
           mov di, offset result
           mov [di], ax
-        
+
           mov ah, 4ch
           int 21h
-        
+
 code ends
-end start
+        end start
