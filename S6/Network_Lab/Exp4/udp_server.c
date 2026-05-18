3bb5d7355a66bab01e08c0298a134f45b0bdf82b
HowlingWolf9 <athul121404@gmail.com>
2026-05-18 15:25:51 +0000

    feat: implement UDP client-server communication with input handling and socket error checking

--- S6/Network_Lab/Exp4/udp_server.c
+++ S6/Network_Lab/Exp4/udp_server.c
@@ -1,33 +1,37 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
-int main()
-{
-    struct sockaddr_in client, server;
-    int lfd, n;
-    char rBuf[100] = "", sBuf[100] = "";
+int main() {
+  struct sockaddr_in client, server;
+  int lfd;
+  char rBuf[100] = "", sBuf[100] = "";
 
-    lfd = socket(AF_INET, SOCK_DGRAM, 0);
-    server.sin_family = AF_INET;
-    server.sin_port = 2001;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  lfd = socket(AF_INET, SOCK_DGRAM, 0);
+  server.sin_family = AF_INET;
+  server.sin_port = 2001;
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    bind(lfd, (struct sockaddr *)&server, sizeof server);
-    printf("\nServer ready, waiting for client....\n");
-    n = sizeof client;
-    recvfrom(lfd, rBuf, sizeof rBuf, 0, (struct sockaddr *)&client, &n);
-
-    printf("\nClient: %s", rBuf);
-    printf("\nServer: ");
-    gets(sBuf);
-    sendto(lfd, sBuf, sizeof sBuf, 0, (struct sockaddr *)&client, n);
-
+  if (bind(lfd, (struct sockaddr *)&server, sizeof server) < 0) {
+    perror("Bind failed.");
     close(lfd);
+    return 1;
+  };
 
-    return 0;
+  printf("\nServer ready, waiting for client....\n");
+  socklen_t n = sizeof client;
+
+  recvfrom(lfd, rBuf, sizeof rBuf, 0, (struct sockaddr *)&client, &n);
+  printf("\nClient: %s", rBuf);
+  printf("\nServer: ");
+  scanf("%s", sBuf);
+  sendto(lfd, sBuf, sizeof sBuf, 0, (struct sockaddr *)&client, n);
+  printf("\n");
+
+  close(lfd);
+  return 0;
 }
