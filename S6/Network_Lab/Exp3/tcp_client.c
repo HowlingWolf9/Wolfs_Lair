77eb61772865c0ca38c5b7949aee28b14e5e2186
HowlingWolf9 <athul121404@gmail.com>
2026-05-18 15:06:30 +0000

    feat: improve TCP implementations with error handling and secure input functions

--- S6/Network_Lab/Exp3/tcp_client.c
+++ S6/Network_Lab/Exp3/tcp_client.c
@@ -1,34 +1,37 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
-int main()
-{
-    struct sockaddr_in server;
-    int lfd;
-    char rBuf[100] = "", sBuf[100] = "";
+int main() {
+  struct sockaddr_in server;
+  int lfd;
+  char rBuf[100] = "", sBuf[100] = "";
 
-    lfd = socket(AF_INET, SOCK_STREAM, 0);
-    server.sin_family = AF_INET;
-    server.sin_port = 2000;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  lfd = socket(AF_INET, SOCK_STREAM, 0);
+  server.sin_family = AF_INET;
+  server.sin_port = 2000;
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    printf("\nClient ready....\n");
-    connect(lfd, (struct sockaddr *)&server, sizeof server);
-
-    printf("\nClient: ");
-    gets(sBuf);
-    send(lfd, sBuf, sizeof sBuf, 0);
-    recv(lfd, rBuf, sizeof rBuf, 0);
-
-    printf("\nServer: %s", rBuf);
-    printf("\n");
-
+  printf("\nClient ready....\n");
+  if (connect(lfd, (struct sockaddr *)&server, sizeof server) < 0) {
+    perror("Connection failed");
     close(lfd);
+    return 1;
+  }
 
-    return 0;
+  printf("\nClient: ");
+  scanf("%s", sBuf);
+  send(lfd, sBuf, sizeof sBuf, 0);
+  recv(lfd, rBuf, sizeof rBuf, 0);
+
+  printf("Server: %s", rBuf);
+  printf("\n\n");
+
+  close(lfd);
+
+  return 0;
 }
--- S6/Network_Lab/Exp3/tcp_server.c
+++ S6/Network_Lab/Exp3/tcp_server.c
@@ -1,38 +1,41 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
-int main()
-{
-    struct sockaddr_in client, server;
-    int lfd, n, confd;
-    char rBuf[100] = "", sBuf[100] = "";
+int main() {
+  struct sockaddr_in client, server;
+  int lfd, confd;
+  char rBuf[100] = "", sBuf[100] = "";
 
-    lfd = socket(AF_INET, SOCK_STREAM, 0);
-    server.sin_family = AF_INET;
-    server.sin_port = 2000;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  lfd = socket(AF_INET, SOCK_STREAM, 0);
+  server.sin_family = AF_INET;
+  server.sin_port = 2000;
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    bind(lfd, (struct sockaddr *)&server, sizeof server);
-    listen(lfd, 1);
-
-    printf("\nServer ready, waiting for client....\n");
-    n = sizeof client;
-    confd = accept(lfd, (struct sockaddr *)&client, &n);
-    recv(confd, rBuf, sizeof rBuf, 0);
-
-    printf("\nClient: %s", rBuf);
-    printf("\nServer: ");
-    gets(sBuf);
-    send(confd, sBuf, sizeof sBuf, 0);
-    printf("\n");
-
-    close(confd);
+  if (bind(lfd, (struct sockaddr *)&server, sizeof server) < 0) {
+    perror("Bind failed.");
     close(lfd);
+    return 1;
+  }
+  listen(lfd, 1);
 
-    return 0;
+  printf("\nServer ready, waiting for client....\n");
+  socklen_t n = sizeof client;
+  confd = accept(lfd, (struct sockaddr *)&client, &n);
+  recv(confd, rBuf, sizeof rBuf, 0);
+
+  printf("\nClient: %s", rBuf);
+  printf("\nServer: ");
+  scanf("%s", sBuf);
+  send(confd, sBuf, sizeof sBuf, 0);
+  printf("\n");
+
+  close(confd);
+  close(lfd);
+
+  return 0;
 }
